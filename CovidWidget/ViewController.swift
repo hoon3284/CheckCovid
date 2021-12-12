606a9dc371172eddbd037796dc3e8f0ce3907d8e
wickedRun <53684146+hoon3284@users.noreply.github.com>
2021-12-12 17:30:33 +0900

    Initial commit

---
+++ CovidWidget/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  CovidWidget
+//
+//  Created by wickedRun on 2021/12/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
+

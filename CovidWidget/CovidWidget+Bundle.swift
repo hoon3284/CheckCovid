57586e4567ae912428987936f29719a5c615e2c2
wickedRun <hoon3284@icloud.com>
2021-12-17 02:35:31 +0900

    Add CovidInfo.plist to hide apiKey

---
+++ CovidWidget/CovidWidget+Bundle.swift
@@ -0,0 +1,21 @@
+//
+//  CovidWidget+Bundle.swift
+//  CovidWidget
+//
+//  Created by wickedRun on 2021/12/17.
+//
+
+import Foundation
+
+extension Bundle {
+    var apiKey: String {
+        guard let file = self.path(forResource: "CovidInfo", ofType: "plist"),
+        let resource = NSDictionary(contentsOfFile: file)
+        else { return "" }
+        guard let key = resource["API_KEY"] as? String else {
+            fatalError("CovidInfo.plist에 API_KEY 값을 넣어주세요.")
+        }
+        
+        return key
+    }
+}
